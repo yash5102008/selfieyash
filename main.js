2f08c0615eaf28ac6cc84367b081ca1b203b0ebf
yash5102008 <75563395+yash5102008@users.noreply.github.com>
2021-09-08 17:52:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition =  new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+    
+}
+
+recognition.onresult = function(event) {
+
+      console.log(event);
+      
+      var Content = event.result[0][0].transcript;
+
+      document.getElementById("textbox").innerHTML = Content;
+      console.log(Content);
+      speak();
+}
+
+  function speak(){
+      var synth = window.SpeechSynthesis;
+
+      speak_data = document.getElementById("textbox").value;
+
+      var utterThis = new speechSynthesisUtterrance(speak_data);
+
+      synth.speak(utterThis);
+      Webcam.attach(camera);
+  }
+
+ Webcam.set({
+      width:360,
+      height:250,
+      image_format : 'png',
+      png_quality:90
+ });
+ camera = document.getElementById("camera");
